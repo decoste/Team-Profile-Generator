8cecfb5b74811fba4ecae019e59f07d9f084ef81
ala decoste <alshareefialaa@gmail.com>
2020-12-03 03:41:21 -0500

    Used inquirer to gather informations, and installed inquirer-loop.

--- Develop/app.js
+++ Develop/app.js
@@ -9,6 +9,94 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+//I installed inquirer-loop. I used the following npm doumentation: "https://www.npmjs.com/package/inquirer-loop"
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
+    inquirer.prompt({
+        questions :[
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the employee name: ",
+                validate: input => {
+                    //https://stackoverflow.com/questions/49633558/javascript-form-validation-letters-only
+                    if (!/^[a-zA-Z]*$/g.test(input)) {
+                        return "Invalid input. The input should be letters only";
+                    }
+                    else return true;
+                }
+            }
+            ,
+            {
+                type: "number",
+                name: "id",
+                message: "Enter the employee ID: "
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Enter the employee email address: ",
+                validate: input => {
+                    //https://stackoverflow.com/questions/4964691/super-simple-email-validation-with-javascript
+                    if (!/(.+)@(.+){2,}\.(.+){2,}/.test(input)) {
+                        return "Invalid email."
+                    }
+                    else return true
+                }
+            },
+            {
+                type: "list",
+                message: "Enter the employee's role: ",
+                name: "role",
+                choices: [Manager, Engineer, Intern]
+            },
+            {
+                type: "input",
+                message: "Enter the engineer's github account: ",
+                name: "github",
+                validate: input => {
+                    if (!/^[a-zA-Z]*$/g.test(input)) {
+                        return "Invalid input. The input should be letters only";
+                    }
+                    else return true;
+                },
+                when: input => input.role === "Engineer"
+            },
+            {
+                type: "number",
+                message: "Enter the manager's office number: ",
+                name: "number",
+                filter: input2 => {
+                    if (!/^[1-9]{1}[0-9]{9}$/.test(input2)) {
+                        return ""
+                    }
+                    else return true
+                },
+                when: input => input.role === "Manager"
+            },
+            {
+                type: "input",
+                message: "Enter the intern's school name: ",
+                name: "github",
+                validate: input => {
+                    if (!/^[a-zA-Z]*$/g.test(input)) {
+                        return "Invalid input. The input should be letters only";
+                    }
+                    else return true;
+                },
+                when: input => input.role === "Intern"
+            }
+
+        ],
+        type: "input",
+        type: "loop",
+        name: "team",
+        message: "Would you like to add an employee to the team?"
+}
+
+)
+.then((response) => {
+    console.log(response);
+})
 
 
 // Write code to use inquirer to gather information about the development team members,
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,8 +1,6 @@
 const Employee = require("./Employee");
 
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-Employee = require("./Employee");
-
 class Manager extends Employee {
     constructor(name, id, email, officeNumber){
         super(name, id, email)
