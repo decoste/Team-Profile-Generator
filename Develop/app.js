37742eae39858cceb08bc19671bc6bcb12ab2da6
ala decoste <alshareefialaa@gmail.com>
2020-12-04 04:53:30 -0500

    Added the prompted questions and the functionality to create a team.html file.

--- Develop/app.js
+++ Develop/app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,94 +10,146 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-//I installed inquirer-loop. I used the following npm doumentation: "https://www.npmjs.com/package/inquirer-loop"
-inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
-    inquirer.prompt({
-        questions :[
-            {
-                type: "input",
-                name: "name",
-                message: "Enter the employee name: ",
-                validate: input => {
-                    //https://stackoverflow.com/questions/49633558/javascript-form-validation-letters-only
-                    if (!/^[a-zA-Z]*$/g.test(input)) {
-                        return "Invalid input. The input should be letters only";
-                    }
-                    else return true;
-                }
-            }
-            ,
-            {
-                type: "number",
-                name: "id",
-                message: "Enter the employee ID: "
-            },
-            {
-                type: "input",
-                name: "email",
-                message: "Enter the employee email address: ",
-                validate: input => {
-                    //https://stackoverflow.com/questions/4964691/super-simple-email-validation-with-javascript
-                    if (!/(.+)@(.+){2,}\.(.+){2,}/.test(input)) {
-                        return "Invalid email."
-                    }
-                    else return true
-                }
-            },
-            {
-                type: "list",
-                message: "Enter the employee's role: ",
-                name: "role",
-                choices: [Manager, Engineer, Intern]
-            },
-            {
-                type: "input",
-                message: "Enter the engineer's github account: ",
-                name: "github",
-                validate: input => {
-                    if (!/^[a-zA-Z]*$/g.test(input)) {
-                        return "Invalid input. The input should be letters only";
-                    }
-                    else return true;
-                },
-                when: input => input.role === "Engineer"
-            },
-            {
-                type: "number",
-                message: "Enter the manager's office number: ",
-                name: "number",
-                filter: input2 => {
-                    if (!/^[1-9]{1}[0-9]{9}$/.test(input2)) {
-                        return ""
-                    }
-                    else return true
-                },
-                when: input => input.role === "Manager"
-            },
-            {
-                type: "input",
-                message: "Enter the intern's school name: ",
-                name: "github",
-                validate: input => {
-                    if (!/^[a-zA-Z]*$/g.test(input)) {
-                        return "Invalid input. The input should be letters only";
-                    }
-                    else return true;
-                },
-                when: input => input.role === "Intern"
-            }
 
-        ],
-        type: "input",
-        type: "loop",
-        name: "team",
-        message: "Would you like to add an employee to the team?"
+const team = [];
+
+function showMenu() {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "name",
+            message: "Enter the employee name: ",
+            validate: input => {
+                //https://stackoverflow.com/questions/49633558/javascript-form-validation-letters-only
+                if (!/^[a-zA-Z]*$/g.test(input)) {
+                    return "Invalid input. The input should be letters only";
+                }
+                else return true;
+            }
+        }
+            ,
+        {
+            type: "number",
+            name: "id",
+            message: "Enter the employee ID: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the employee email address: ",
+        },
+        {
+            type: "list",
+            message: "Enter the employee's role: ",
+            name: "role",
+            choices: [Manager, Engineer, Intern]
+        },
+        {
+            type: "input",
+            message: "Enter the engineer's github account: ",
+            name: "github",
+            validate: input => {
+                if (!/^[a-zA-Z]*$/g.test(input)) {
+                    return "Invalid input. The input should be letters only";
+                }
+                else return true;
+            },
+            when: input => input.role === "Engineer"
+        },
+        {
+            type: "number",
+            message: "Enter the manager's office number: ",
+            name: "officeNumber",
+            filter: input2 => {
+                if (!/^[1-9]{1}[0-9]{9}$/.test(input2)) {
+                    return ""
+                }
+                else return true
+            },
+            when: input => input.role === "Manager"
+        },
+        {
+            type: "input",
+            message: "Enter the intern's school name: ",
+            name: "school",
+            validate: input => {
+                if (!/^[a-zA-Z]*$/g.test(input)) {
+                    return "Invalid input. The input should be letters only";
+                }
+                else return true;
+            },
+            when: input => input.role === "Intern"
+        },
+        {
+           // Reference: "https://stackoverflow.com/questions/58442756/nodejs-how-to-re-display
+           //-custom-cli-menu-after-executing-corresponding-function"
+            name: "back",
+            type: "input",
+            message: "Would you like to add another employee?(y or n)",
+            choices: ["y", "n"],
+        }]
+        ).then((input) => {
+            
+            if (input.role === "Manager") {
+                const emp = new Manager(
+                    input.name,
+                    input.id,
+                    input.email,
+                    input.officeNumber
+                );
+                team.push(emp)
+            }
+ 
+            else if (input.role === "Intern") {
+                const emp = new Intern(
+                    input.name,
+                    input.id,
+                    input.email,
+                    input.school
+                );
+                team.push(emp)
+            }
+            else if (input.role === "Engineer") {
+                const emp = new Engineer(
+                    input.name,
+                    input.id,
+                    input.email,
+                    input.officeNumber
+                );
+                team.push(emp)
+            }
+            if (input.back === 'y'){
+                return showMenu();
+            }
+            else {
+                const renderteam = render(team);
+
+            console.log(input);
+
+            console.log(team);
+
+            
+            fs.writeFile(outputPath, renderteam, {}, (err) =>
+                err ? console.log(err) : console.log("HTML file created")
+            );
+            //create HTML file
+
+        }
+    }
+        )
+
+        .catch((err) => {
+            console.log(err);
+        });
+
 }
 
-)
-.then((response) => {
-    console.log(response);
-})
+showMenu();
+
+
+
+
+
 
 
 // Write code to use inquirer to gather information about the development team members,
